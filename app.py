9e1b3cafcf7ed467fc6854149f424ee3d4a44685
Nardos-serkalem <nadii8044@gmail.com>
2025-05-30 14:18:37 +0300

    add

--- app.py
+++ app.py
@@ -1,59 +1,63 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Home page (index.html)
+db = SQLAlchemy(app)
+
+from models import User
+
+
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
-# Authentication pages
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-# Other main pages
-@app.route('/news')
-def news():
-    return render_template('news.html')
-
-@app.route('/partnership')
-def partnership():
-    return render_template('partnership.html')
-
-@app.route('/products')
-def products():
-    return render_template('products.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/services')
 def services():
     return render_template('services.html')
 
-@app.route('/testimonial')
-def testimonial():
-    return render_template('testimonial.html')
+@app.route('/testimonials')
+def testimonials():
+    return render_template('testimonials.html')
+
+@app.route('/partnership')
+def partnership():
+    return render_template('partnership.html')
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-# About and its subpages
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/awards')
+def awards():
+    return render_template('awards.html')
 
-@app.route('/about/discover')
-def discover_us():
-    return render_template('discover_us.html')
+@app.route('/news')
+def news():
+    return render_template('news.html')
 
-@app.route('/about/team')
-def executive_team():
-    return render_template('executive_team.html')
+@app.route('/discover')
+def discover():
+    return render_template('discover.html')
 
+@app.route('/overview')
+def overview():
+    return render_template('overview.html')
 
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,8 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
